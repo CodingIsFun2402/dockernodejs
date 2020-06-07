28ce2dabbb4f803e9c7d7e36fcabaca1815b9e4a
CodingIsFun2402 <61429007+CodingIsFun2402@users.noreply.github.com>
2020-06-07 22:21:55 +0200

    first upload from HDD

---
+++ app.js
@@ -0,0 +1,26 @@
+function Person(vorname, nachname) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+}
+
+Person.prototype.getFullname = function() {
+    return this.vorname + " " + this.nachname;
+}
+
+function Schueler(vorname, nachname, schule) {
+    // Die call-function tauscht den Kontext this aus und ruft die Funktion auf.
+    Person.call(this, vorname, nachname);
+    this.schule = schule;
+}
+Schueler.prototype.getFullname = function() {
+    return this.vorname + " " + this.nachname + "(" + this.schule + ")";
+}
+
+Schueler.prototype.__proto__ = Person.prototype;
+
+var hans = new Person("Hans", "Guckindieluft");
+var jannick = new Schueler("Jannick", "Moritz", "Elisabethenschule");
+console.log(hans);
+console.log(jannick);
+console.log(hans.getFullname());
+console.log(jannick.getFullname());
---
+++ start.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+var counter = 0;
+
+app.get("/", function( req, res) {
+    var message = "Anzahl Zugriffe: ";
+    counter++;
+    message = message + counter;
+    res.send();
+});
+
+app.listen(3300, function() {
+    console.log("app lauscht auf Port 3300");
+});
