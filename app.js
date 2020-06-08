228bcbb5472372ab827b00c21a16f820d5846f2f
Marko Moritz <java.marko.moritz@gmail.com>
2020-06-08 23:25:10 +0200

    Routen, Middleware Parameter

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-function Person(vorname, nachname) {
-    this.vorname = vorname;
-    this.nachname = nachname;
-}
+'use strict';
+// Module aus Node laden
+const http = require('http');
+const path = require('path');
 
-Person.prototype.getFullname = function() {
-    return this.vorname + " " + this.nachname;
-}
+// Module über NPM laden
+const express = require('express');
 
-function Schueler(vorname, nachname, schule) {
-    // Die call-function tauscht den Kontext this aus und ruft die Funktion auf.
-    Person.call(this, vorname, nachname);
-    this.schule = schule;
-}
-Schueler.prototype.getFullname = function() {
-    return this.vorname + " " + this.nachname + "(" + this.schule + ")";
-}
+// Module lokal laden
+// Eine Middleware, diese hier übernimmt das Loggen in die Konsole
+const logger = require('./logger');
 
-Schueler.prototype.__proto__ = Person.prototype;
+const clientDirectory = path.join(__dirname, 'client');
+const app = express();
 
-var hans = new Person("Hans", "Guckindieluft");
-var jannick = new Schueler("Jannick", "Moritz", "Elisabethenschule");
-console.log(hans);
-console.log(jannick);
-console.log(hans.getFullname());
-console.log(jannick.getFullname());
+app.use(logger({ level: 'warn' }));
+
+// Eine Middleware, diese hier liefert statische Seiten aus
+app.use('/', express.static(clientDirectory));
+
+//Zugriff auf URL Parameter 
+app.get('/depot/:nr', (req, res) => {
+    //Zugriff auf Query ?format=
+    if(req.query.format === 'html') {
+        // http://localhost:3002/depot/1?format=html
+        res.send(`<h1>Anzeige Depot ${req.params.nr}</h1>`);
+        return;
+    } else if (req.query.format === 'json') {
+        //  http://localhost:3002/depot/1?format=json
+        const depot = { nr: `${req.params.nr}` };
+        depot
+        res.send(depot);
+        return;
+    }
+    res.send("Depot Nr: " + `${req.params.nr}`);
+});
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3002, () => {
+    console.log('HTTP Server is listen on port 3002')
+});
